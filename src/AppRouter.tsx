3e3e23255f1c20d43daf414f00636e9af52aacc4
RodrigoZamberlan <rodrigozfreddo@outlook.com>
2024-12-03 11:32:26 -0300

    add router

---
+++ src/AppRouter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const AppRouter: React.FC = () => {
+    return (<Router>
+                <Routes>
+                    <Route></Route>
+                </Routes>
+            </Router>);
+}
+
+export default AppRouter;
